1169c96893c00de99fa35031858dd10241bf6a81
unknown <rifkifikri000@gmail.com>
2021-02-20 05:15:17 -0800

    sorting array

--- sorting array.cpp
+++ sorting array.cpp
@@ -1,22 +1,45 @@
 #include<iostream>
-#include<algorithm>
+
 using namespace std;
+void output(int arr[], int input, int lengh){
+	int tampung;
+	for(int x=1;x < lengh;x++){
+		for(int y=0; y< lengh-x;y++){
+			if(arr[y]>arr[y+1]){
+				tampung=arr[y];
+				arr[y]=arr[y+1];
+				arr[y+1]=tampung;
+			}
+			
+		}
+		for(int c=0;c <lengh; c++){
+		cout<<arr[c]<<" | ";
+		}
+		cout<<endl;
+	
+	}
+	cout<<"Hasil Sorting nya adalah : ";
+		for(int x=0;x< lengh;x++){
+			cout<<arr[x]<<" ";
+		}
+}
 
 int main(){
-	int arr[10], y=arr[10];
+	int input;
 	
-	for(int x=0;x<10;x++){
-	cout<<"inputkan nilai :";
-	cin>> arr[x];	
+	cout<<"Masukkan Panjang array :";
+	cin >> input;
+	int arr[input];
+	int lengh=sizeof(arr)/sizeof(*arr);
+	
+	for(int a=0;a < input;a++){
+		cout<<"Masukkan Nilai ke- "<<a<<" :";
+		cin>>arr[a];
 	}
-	cout<<endl;
-	for(int x=0;x<10;x++){
-		y=arr[x];
-	cout<<y<<", ";
-	cout<<endl;
-	sort(y.begin(), y.end());
-	cout<<y<<", ";
-	}
+	
+
+	output(arr,input,lengh);
 	
 	
 }
+
